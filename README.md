6d7b25b8846476802d781ecf421358ec175d6436
dajkim76 <dajkim76@naver.com>
2026-08-30 01:50:25 +0900

    라이브러리 버전을 1.0.1 로

--- README.md
+++ README.md
@@ -59,7 +59,7 @@ dependencyResolutionManagement {
 
 ```kotlin
 dependencies {
-    implementation("com.github.dajkim76:DiffView:1.0.0")
+    implementation("com.github.dajkim76:DiffView:1.0.1")
 }
 ```
 
--- diffview/build.gradle.kts
+++ diffview/build.gradle.kts
@@ -62,7 +62,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dajkim76"
                 artifactId = "DiffView"
-                version = "1.0.0"
+                version = "1.0.1"
             }
         }
     }
